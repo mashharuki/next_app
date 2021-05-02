8193d57f3b08268f3530e46c98e778e428c1a989
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-02 17:07:12 +0900

    レイアウトを用いて遷移させることに成功しました。

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,25 @@
 // 各種コンポーネントを読み込む。
-import Header from './components/Header';
 import Link from 'next/link';
+import Layout from './components/layout';
 
 // デフォルトコンポーネント
 export default function Home() {
-  // 変数
-  const title = "Index";
 
   // レンダリング
   return (
     <div>
-      <Header title={title} />
-      <h1 className="bg-primary text-white display-4 px-3">
-        React
-      </h1>
-      <div className="container">
-        <h3 className="my-3 text-primary text-center">
-          {title}
-        </h3>
-        <div className="card p-3 text-center">
-          <p>
-            これは、ページ移動のサンプルです。
-          </p>
-          <Link href="/other">
-            <a>
+      <Layout header="Next.js" title="Top page.">
+        <div className="alert alert-primary text-center">
+          <h5 className="mb-4">
+            Welcome to next.js!
+          </h5>
+          <Link href="./other">
+            <button className="btn btn-primary">
               Go to Other page &gt;&gt;
-            </a>
+            </button>
           </Link>
         </div>
-      </div>
+      </Layout>
     </div>
-  )
+  );
 }
--- pages/other.js
+++ pages/other.js
@@ -1,5 +1,5 @@
 // 各種コンポーネントを読み込む。
-import Header from './components/Header';
+import Layout from './components/layout';
 import Link from 'next/link';
 
 // Otherコンポーネント
@@ -10,25 +10,18 @@ export default function Other() {
     // レンダリング
     return (
         <div>
-            <Header title={title} />
-            <h1 className="bg-primary text-white display-4 px-3">
-                React
-            </h1>
-            <div className="container">
-                <h3 className="my-3 text-primary text-center">
-                {title}
-                </h3>
-                <div className="card p-3">
-                    <p>
-                        これは、もう一つのページです。
-                    </p>
-                    <Link href="/">
-                        <a>
-                            &lt;&lt; Back to Index page 
-                        </a>
-                    </Link>
-                </div>
-            </div>
+         <Layout header="Next.js" title="Other page.">
+           <div className="card p-4 text-center">
+             <h5 className="mb-4">
+               This is Other page...
+             </h5>
+             <Link href=".">
+               <button className="btn btn-primary">
+                    &lt;&lt; Back to Top 
+               </button>
+             </Link>
+           </div>
+         </Layout>
         </div>
     );
 }
