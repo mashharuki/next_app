1f596d3fdac82e9689463eb427ff869fa9f1d5a7
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-02 22:26:53 +0900

    funcモジュールとAPIを作成

---
+++ pages/api/func.js
@@ -0,0 +1,8 @@
+// funcモジュールを外部から使えるようにAPI化する。
+import func from '../components/func';
+
+// handler関数コンポーネント
+export default function handler(req, res) {
+    // funcモジュールをjson形式で返す。s
+    res.json(func);
+}
