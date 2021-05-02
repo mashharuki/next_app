218873fc159ac9acec53d1daec46c4da43a6cddc
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-02 14:58:08 +0900

    コメントを追加しました。

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+// 各種コンポーネントを読み込む。
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+// デフォルトコンポーネント
 export default function Home() {
   return (
     <div className={styles.container}>
