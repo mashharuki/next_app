80e8e22d3c7204b35d13eb8c60c363d23c71ba0b
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-02 20:47:44 +0900

    SWRを利用してデータを扱うように変更しました。

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,12 @@ import Link from 'next/link';
 import Layout from './components/layout';
 import MyImage from './components/images';
 import { useState } from 'react';
+import useSWR from 'swr';
 
 // デフォルトコンポーネント
 export default function Home() {
   // 変数
-  const url = './data.json';
-  const [ data, setData ] = useState({ message:'', data:[] });
-
-  // fecth APIでデータを扱う
-  // データを取得し終わったら非同期処理を行う。
-  fetch(url)
-    .then(res => res.json())
-    .then(res => setData(res))
+  const { data } = useSWR('./data.json');
 
   // レンダリング
   return (
@@ -22,10 +16,10 @@ export default function Home() {
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">
-            {data.message}
+            {data != undefined ? data.message : 'error...' }
           </h5>
           <table className="table bg-white">
-            <thead className="table-dark">
+            <thead className="">
               <tr>
                 <th>
                   Name
@@ -39,7 +33,7 @@ export default function Home() {
               </tr>
             </thead>
             <tbody>
-              {data.data.map((value, key) => (
+              {data != undefined ? data.data.map((value, key) => (
                 <tr key={key}>
                   <th>
                     {value.name}
@@ -51,7 +45,8 @@ export default function Home() {
                     {value.age}
                   </td>
                 </tr>
-              ))}
+              )) 
+              : <tr><th></th><td>no data.</td><td></td></tr> }
             </tbody>
           </table>
         </div>
