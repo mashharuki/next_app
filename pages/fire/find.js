16ca09df4f6c4164d03c99cda625386de48173c6
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-03 16:04:23 +0900

    検索機能を付け加えました。

--- pages/fire/find.js
+++ pages/fire/find.js
@@ -10,6 +10,52 @@ const db = firebase.firestore();
 
 // Findコンポーネント
 export default function Find () {
+    // ステート変数を用意する。
+    const [ message, setMessage ] = useState('find data');
+    const [ find, setFind ] = useState('');
+    const [ data, setData ] = useState([]);
+    // 配列用のデータ
+    const mydata = [];
+
+    // 検索条件が変化した時の処理
+    const onChangeFind = ((e) => {
+        // ステート変数を更新する。
+        setFind(e.target.value);
+    });
+
+    // ボタン押した時の処理
+    const doAction = ((e) => {
+        // データを取得する。(getメソッド＋where句)
+        db.collection('mydata').where('name', '==', find).get().then( snapshot => {
+            // 取り出したデータを順に処理する。
+            snapshot.forEach((document) => {
+                // データを取り出す。
+                const doc = document.data();
+                // 配列mydataに追加する。
+                mydata.push(
+                    <tr key={document.id}>
+                        <td>
+                            <a href={'/fire/del?id=' + document.id}>
+                                {document.id}
+                            </a>
+                        </td>
+                        <td>
+                            {doc.name}
+                        </td>
+                        <td>
+                            {doc.mail}
+                        </td>
+                        <td>
+                            {doc.age}
+                        </td>
+                    </tr>
+                );
+            });
+            // ステート変数にセットする。
+            setData(mydata);
+            setMessage('find: ' + find);
+        });
+    });
 
     // レンダリングする。
     return (
@@ -19,20 +65,31 @@ export default function Find () {
                     <h5 className="mb-4">
                         {message}
                     </h5>
-                    <table className="table bg-white text-left">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Mail</th>
-                                <th>Age</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data}
-                        </tbody>
-                    </table>
+                    <div className="text-left">
+                        <div className="form-group">
+                            <label>
+                                Find:
+                            </label>
+                            <input type="text" onChange={onChangeFind} className="form-control"/>
+                        </div>
+                    </div>
+                    <button onClick={doAction} className="btn btn-primary">
+                        Find
+                    </button>
                 </div>
+                <table className="table bg-white text-left">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Mail</th>
+                            <th>Age</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data}
+                    </tbody>
+                </table>
             </Layout>
         </div>
     );
